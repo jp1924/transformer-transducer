cd47639389c2a60332c9198aa10f6280cacdd18c
jp1924 <jp42maru@gmail.com>
2022-10-24 10:35:24 +0900

    Add: some structure

---
+++ main.py
@@ -0,0 +1,20 @@
+from setproctitle import setproctitle
+import argparse
+import torch
+
+
+def train_step() -> torch.Tensor:
+    return
+
+
+def tokenizer() -> torch.Tensor:
+    return
+
+
+def main() -> None:
+    None
+
+
+if "__main__" in __name__:
+    setproctitle("[JP]torch.ver bert")
+    main()

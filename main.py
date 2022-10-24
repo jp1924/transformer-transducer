0f2e9e048e217c8a6393371ffb521991183e95fe
jp1924 <jp42maru@gmail.com>
2022-10-24 11:32:43 +0900

    Add: args, model, config, tokenizer from HF

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from setproctitle import setproctitle
 import argparse
 import torch
 
+from utils import TrainArguments, ModelArguments, DataArguments
 
-def train_step() -> torch.Tensor:
-    return
-
-
-def tokenizer() -> torch.Tensor:
-    return
+from transformers import BertForSequenceClassification, BertTokenizerFast, BertConfig, HfArgumentParser
+from datasets import load_dataset
 
 
-def main() -> None:
-    None
+def main(parser: HfArgumentParser) -> None:
+    train_args, model_args, data_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
+
+    setproctitle("[JP]torch.ver bert")
+
+    tokenizer = BertTokenizerFast.from_pretrained("klue/bert-base", cache_dir=train_args.cache)
+    config = BertConfig.from_pretrained(
+        "klue/bert-base",
+        cache_dir=train_args.cache,
+        vocab_size=tokenizer.vocab_size,
+        num_labels=model_args.num_labels,
+    )
+    model = BertForSequenceClassification.from_pretrained("klue/bert-base", cache_dir=train_args.cache, config=config)
 
 
 if "__main__" in __name__:
-    setproctitle("[JP]torch.ver bert")
-    main()
+    parser = HfArgumentParser([TrainArguments, ModelArguments, DataArguments])
+    main(parser)

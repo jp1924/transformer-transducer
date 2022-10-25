f9ee64028a5115ae2fe01bf62557337fb003fb6b
jp1924 <jp42maru@gmail.com>
2022-10-25 14:55:07 +0900

    Clean: trainer dir > trainer.py

---
+++ trainer.py
@@ -0,0 +1,21 @@
+class TorchTraner:
+    def __init__(self, model, tokenizer, train_data, valid_data) -> None:
+        return
+
+    def train(self) -> None:
+        return
+
+    def train_step(self) -> None:
+        return
+
+    def valid(self) -> None:
+        return
+
+    def valid_step(self) -> None:
+        return
+
+    def predict(self) -> None:
+        return
+
+    def predict_step(self) -> None:
+        return

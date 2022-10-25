78fba35555d4933ce7bd9e07d6befa994a765dd2
jp1924 <jp42maru@gmail.com>
2022-10-25 14:55:39 +0900

    Add: TorchCollator & TorchSampler

---
+++ data/collator.py
@@ -0,0 +1,9 @@
+from torch.nn.utils.rnn import pad_sequence
+
+
+class TorchCollator:
+    def __init__(self) -> None:
+        None
+
+    def __call__(self) -> None:
+        return
---
+++ data/sampler.py
@@ -0,0 +1,6 @@
+class TorchSampler:
+    def __init__(self, data_source) -> None:
+        pass
+
+    def __iter__(self):
+        raise NotImplementedError

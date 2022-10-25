b06bc4f74e0d118c7f061517bef39d2367736f02
jp1924 <jp42maru@gmail.com>
2022-10-25 14:20:20 +0900

    Add: Basic train & valid loop

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import torch
 
 from utils import TrainArguments, ModelArguments, DataArguments
 
-from transformers import BertForSequenceClassification, BertTokenizerFast, BertConfig, HfArgumentParser
+from transformers import BertForSequenceClassification, BertTokenizerFast, BertConfig, HfArgumentParser, trainer
 from datasets import load_dataset
+from torch import optim
+from torch.utils.data import DataLoader
+from torchaudio.functional import rnnt_loss
+import wandb
 
 
 def main(parser: HfArgumentParser) -> None:
     train_args, model_args, data_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
-
     setproctitle("[JP]torch.ver bert")
 
     tokenizer = BertTokenizerFast.from_pretrained("klue/bert-base", cache_dir=train_args.cache)
@@ -21,6 +24,40 @@ def main(parser: HfArgumentParser) -> None:
         num_labels=model_args.num_labels,
     )
     model = BertForSequenceClassification.from_pretrained("klue/bert-base", cache_dir=train_args.cache, config=config)
+
+    loaded_data = load_dataset("NSMC", cache_dir=train_args.cache)
+    train_data = loaded_data["train"]
+    valid_data = loaded_data["eval"]
+
+    train_dataloader = DataLoader(
+        dataset=train_data,
+        batch_size=train_args.train_batchsize,
+        shuffle=False,
+        batch_sampler=None,
+        collate_fn=None,
+        pin_memory=True,
+    )
+
+    valid_dataloader = DataLoader(
+        dataset=valid_data,
+        batch_size=16,
+        shuffle=False,
+        batch_sampler=None,
+        collate_fn=None,
+        pin_memory=True,
+    )
+
+    optimizer = optim.Adam(model.parameters(), train_args.learning_rate)
+
+    for epoch in range(train_args.train_epochs):
+        for step, train_data in enumerate(range(train_dataloader)):
+
+            outputs = model()
+            loss = rnnt_loss()
+            optimizer.backword(loss)
+            with torch.no_grad():
+                for step, valid_data in enumerate(range(valid_dataloader)):
+                    pass
 
 
 if "__main__" in __name__:

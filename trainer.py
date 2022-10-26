40929f3ebc60d283e94b49ae430cc2849bfcd78d
jp1924 <jp42maru@gmail.com>
2022-10-26 17:14:10 +0900

    Add: set train, valid dataloader

--- trainer.py
+++ trainer.py
@@ -1,11 +1,32 @@
+import torch
+from torch.utils.data import DataLoader
+from argparse import Namespace
+
+
 class TorchTraner:
-    def __init__(self, model, tokenizer, train_data, valid_data) -> None:
+    def __init__(self, model, optimizer, tokenizer, train_data, valid_data, collator, args: Namespace) -> None:
+        self.train_data = train_data
+        self.valid_data = valid_data
+        self.collator = collator
+        self.args = args
+        self.model = model
+        self.optimizer = optimizer
         return
 
     def train(self) -> None:
+        self.gradient_accumulate: int = lambda step: (step % self.args.train_gradient_accumulation) == 0
         return
 
-    def train_step(self) -> None:
+    def train_step(self, step, train_data) -> None:
+
+        outputs = self.model(**train_data)
+        loss = outputs.loss
+
+        if self.gradient_accumulate(step):
+            loss.backward()
+            self.optimizer.step()
+        
+            
         return
 
     def valid(self) -> None:
@@ -18,4 +39,28 @@ class TorchTraner:
         return
 
     def predict_step(self) -> None:
+        return
+
+    def set_train_dataloader(self) -> DataLoader:
+        train_dataloader = DataLoader(
+            dataset=self.train_data,
+            batch_size=self.args.train_batch_size,
+            shuffle=False,
+            batch_sampler=None,
+            collate_fn=self.collator,
+            pin_memory=True,
+        )
+
+        return
+
+    def set_valid_datalaoder(self) -> DataLoader:
+        valid_dataloader = DataLoader(
+            dataset=self.valid_data,
+            batch_size=self.args.valid_batch_size,
+            shuffle=False,
+            batch_sampler=None,
+            collate_fn=self.collator,
+            pin_memory=True,
+        )
+
         return

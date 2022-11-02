6e651dbec641c47fc873f420497792f2189499d5
jp1924 <jp42maru@gmail.com>
2022-11-02 16:56:20 +0900

    Add: label, audio encoder class

--- model/modeling_T_T.py
+++ model/modeling_T_T.py
@@ -2,6 +2,16 @@ import torch
 import torch.nn as nn
 
 
+class LabelEncoder(nn.Module):
+    def __init__(self) -> None:
+        pass
+
+
+class AudioEncoder(nn.Module):
+    def __init__(self) -> None:
+        pass
+
+
 class TransformerTranducer(nn.Module):
     def __init__(self):
         pass

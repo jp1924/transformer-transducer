026ded2d697e70aa8a04c310900e249cb2039eb6
jp1924 <jp42maru@gmail.com>
2022-11-02 16:53:08 +0900

    Add: gradient cliping

--- main.py
+++ main.py
@@ -5,20 +5,49 @@ import wandb
 from datasets import load_dataset
 from setproctitle import setproctitle
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, DistributedSampler
 from torchaudio.functional import rnnt_loss
 from transformers import (
     BertConfig,
     BertForSequenceClassification,
     BertTokenizerFast,
     HfArgumentParser,
-    trainer,
-    BertTokenizer,
+    Trainer,
 )
+from transformers.optimization import AdamW
+from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
+from transformers.trainer_pt_utils import LengthGroupedSampler, DistributedLengthGroupedSampler
 import datasets
 from data import TorchCollator, TorchSampler
-from utils import DataArguments, ModelArguments, TrainArguments
+from utils import DataArguments, ModelArguments, TrainArguments, get_linear_schedule_with_warmup
 import torch.nn as nn
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from data.collator import BertHeatmapCollator
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", default=-1)
+    args = parser.parse_args()
+
+    return args
+
+
+def get_parameter_names(model, forbidden_layer_types):
+    """
+    Returns the names of the model parameters that are not inside a forbidden layer.
+    """
+    result = []
+    for name, child in model.named_children():
+        result += [
+            f"{name}.{n}"
+            for n in get_parameter_names(child, forbidden_layer_types)
+            if not isinstance(child, tuple(forbidden_layer_types))
+        ]
+    # Add model specific parameters (defined with nn.Parameter) since they are not in any child.
+    result += list(model._parameters.keys())
+    return result
 
 
 def main(parser: HfArgumentParser) -> None:
@@ -48,8 +77,24 @@ def main(parser: HfArgumentParser) -> None:
         cache_dir=train_args.cache,
         vocab_size=tokenizer.vocab_size,
         num_labels=model_args.num_labels,
+        pad_token_id=tokenizer.pad_token_id,
+        # initializer_range=0.2,
     )
     model = BertForSequenceClassification.from_pretrained("klue/bert-base", cache_dir=train_args.cache, config=config)
+
+    if train_args.local_rank != -1:
+        torch.cuda.set_device(train_args.local_rank)
+
+        device = torch.device(train_args.local_rank)
+        device_count = torch.cuda.device_count()
+        torch.cuda.set_device(device.index)
+        model = model.to(device)
+
+        model = DistributedDataParallel(
+            model,
+            device_ids=[train_args.local_rank] if device_count != 0 else None,
+            output_device=train_args.local_rank if device_count != 0 else None,
+        )
 
     loaded_data = load_dataset("nsmc", cache_dir=train_args.cache)
     train_data = loaded_data["train"]
@@ -61,17 +106,30 @@ def main(parser: HfArgumentParser) -> None:
     train_data = train_data.remove_columns(["id", "document"])
     valid_data = valid_data.remove_columns(["id", "document"])
 
-    collator = TorchCollator(pad=0)
+    if train_args.local_rank != -1:
+        sampler = DistributedSampler(
+            dataset=train_data,
+            num_replicas=2,
+            rank=train_args.local_rank,
+            shuffle=False,
+            seed=42,
+            drop_last=False,
+        )
+    generator = torch.Generator()
+    generator.manual_seed(42)
+
+    collator = TorchCollator(pad=0, device=train_args.local_rank)
 
     train_dataloader = DataLoader(
         dataset=train_data,
         batch_size=train_args.train_batch_size,
         shuffle=False,
+        # sampler=sampler,
         batch_sampler=None,
         collate_fn=collator,
         pin_memory=True,
+        generator=generator,
     )
-
     valid_dataloader = DataLoader(
         dataset=valid_data,
         batch_size=train_args.valid_batch_size,
@@ -81,19 +139,37 @@ def main(parser: HfArgumentParser) -> None:
         pin_memory=True,
     )
 
-    optimizer = optim.Adam(model.parameters(), train_args.learning_rate)
-    cross_entropy_loss = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), train_args.learning_rate)
+    model.zero_grad()
 
-    for epoch in range(train_args.train_epochs):
+    for epoch in range(1, (train_args.train_epochs + 1)):
+        # set_epoch는 sampler의 shuffle이 True일 때를 위한 함수다.
+        # 만약 set_epoch이 설정되지 않았다면 매 epoch마다 동일한 데이터가 들어갈 수 있다.
+        # random_seed를 설정할 때 seed + epoch으로 seed를 설정하기 때문에 shuffle시 중요한 요소
+        # train_dataloader.sampler.set_epoch(epoch)
+        model.train()
         for step, train_data in enumerate(train_dataloader, 1):
-
-            outputs = model(**train_data)
-            logits = outputs.logits
-            loss = cross_entropy_loss(logits, train_data["labels"])
+            if train_args.local_rank != -1:
+                train_data = {key: data.to(train_args.local_rank) for key, data in train_data.items()}
+                with model.no_sync():
+                    outputs = model(**train_data)
+            else:
+                train_data = {key: data for key, data in train_data.items()}
+                outputs = model(**train_data)
+            loss = outputs.loss
+            # logits = outputs.logits
+            # loss = cross_entropy_loss(logits, train_data["labels"])
             # loss = rnnt_loss()
-            if (step % train_args.train_gradient_accumulation) == 0:
-                loss.backward()
+            loss = loss / train_args.train_accumulate
+            loss.backward()
+            # scheduler.step()
+            if (step % train_args.train_accumulate) == 0:
+                nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step()
+                model.zero_grad()
+
+                print(f"step: {step*epoch}: {loss}")
+                print
 
             if (step % train_args.valid_step) == 0:
 

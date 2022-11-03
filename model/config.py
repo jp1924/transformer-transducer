9814b44178d1908fc7617a2afa5552a1034c60d4
jp1924 <jp42maru@gmail.com>
2022-11-03 16:30:41 +0900

    Add: model config

--- model/config.py
+++ model/config.py
@@ -1,3 +1,15 @@
 class TransformerTransducerConfig:
     def __init__(self) -> None:
-        pass
+        self.label_layers = 8
+        self.sudio_layers = 8
+
+        self.hidden_size = 756
+        self.ffn_size = 3072
+        self.ffn_dropout = 0.03
+        self.ffn_norm_eps = 0.00001
+
+        self.attn_dropout = 0.03
+        self.attn_norm_eps = 0.00001
+        self.score_dropout = 0.07
+
+        self.position_embed_size = 512

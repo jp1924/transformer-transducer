2da26127c8fc25b4255bc47b3b62413ba70db2e7
jp1924 <jp42maru@gmail.com>
2022-11-25 11:57:07 +0900

    Add: test for attention_mask

--- data/collator.py
+++ data/collator.py
@@ -2,6 +2,7 @@ from transformers import PreTrainedTokenizer
 from torch.nn.utils.rnn import pad_sequence
 import torch
 from typing import List, Dict, Any
+import numpy as np
 
 
 class TorchCollator:
@@ -30,7 +31,7 @@ class TorchCollator:
 
 class TransducerCollator:
 
-    max_length: int = 512
+    max_length: int = 504
 
     def __init__(self, tokenizer: PreTrainedTokenizer = None) -> None:
         """DataLoader로 부터 건내받은 불규칙한 길이의 데이터를 일정한 길이오 만든 뒤 model에 건내주는 역할을 합니다.
@@ -38,10 +39,10 @@ class TransducerCollator:
         Transformer Transducer에서 처리하기 위해 Audio데이터를 처리합니다.
 
         **dataset의 형식**
-            "input_values": torch.Tensor(audio_data), shape(1, chnnel, time)
-            "grapheme_labels": torch.Tensor(grapheme_labels data)
-            "syllable_labels": torch.Tensor(syllable_labels data)
-            "lengths": torch.Tensor(length_data)
+            "input_values": numpy.2darray, shape(1, chnnel, time)
+            "input_ids": numpy.1darray
+            "audio_len": int
+            "label_len": int
 
         현재 데이터에서 syllable이 오타가 있어서 syllabel이 되었음
         데이터는 다른 곳에서 이미 전처리가 된 상태로 만들어짐
@@ -58,12 +59,15 @@ class TransducerCollator:
         input_values: list = feature_select("input_values")
         labels: list = feature_select("labels")
 
-        labels = [
-            {"input_values": torch.cat([torch.tensor([0]), tensor["input_ids"][:511]], dim=0)} for tensor in labels
-        ]
+        labels = [{"input_values": np.insert(label, 0, 0)} for label in labels]
 
-        batch = self._audio_pad_1(input_values)
-        labels = self.tokenizer.pad(labels, return_attention_mask=True)
+        batch = self._audio_pad_2(input_values)
+        labels = self.tokenizer.pad(
+            labels,
+            return_attention_mask=True,
+            max_length=self.max_length,
+            return_tensors="pt",
+        )
 
         batch["labels"] = labels["input_values"]
         batch["label_attention_mask"] = labels["attention_mask"]
@@ -75,7 +79,7 @@ class TransducerCollator:
 
         # [NOTE]: cutting by max_length
         input_values = [value[:, :, : self.max_length] for value in input_values]
-        max_size = max([value.shape[2] for value in input_values])
+        max_size = 504  # max([value.shape[2] for value in input_values])
 
         difference = [max_size - value.shape[2] for value in input_values]
 
@@ -104,31 +108,31 @@ class TransducerCollator:
         return result
 
     def _audio_pad_2(self, input_values: List[torch.Tensor]) -> torch.Tensor:
-        return_result = list()
-        input_values = [value[:, :, : self.max_length] for value in input_values]
-        # max_size = max([value.shape[2] for value in input_values])
-        max_size = 504
+        max_length = 512
+        input_values = [value[:max_length, :] for value in input_values]
 
-        difference = [max_size - value.shape[2] for value in input_values]
-        chennel_size = input_values[0].shape[1]
-        padded_values = list()
-        for pad_size, value in zip(difference, input_values):
-            pad = torch.zeros([1, chennel_size, pad_size], dtype=torch.float64)
-            value = torch.cat([value, pad], dim=2)
-            padded_values.append(value)
-        audios = torch.cat(padded_values, dim=0)
-
+        pad_result = list()
         for audio in input_values:
-            seq_len = audio.size(1)
-            up = torch.triu(audio.new_ones([seq_len, seq_len]), diagonal=2 + 1)
-            down = torch.tril(audio.new_ones([seq_len, seq_len]), diagonal=-10 - 1)
+            time_seq, mel_seq = audio.shape
+            pad_zeros_mat = np.zeros([max_length - time_seq, mel_seq])
+            padded_inputs = np.row_stack([audio, pad_zeros_mat])
+            pad_result.append(padded_inputs)
+
+        pad_result = torch.tensor(pad_result)
+
+        attention_result = list()
+        for audio in input_values:
+            time_seq, mel_seq = audio.shape
+            mask = np.ones([mel_seq, mel_seq])
+            up = np.triu(mask, k=2 + 1)
+            down = np.tril(mask, k=-10 - 1)
             attention_mask = up + down
-            return_result.append(attention_mask)
-        return_result = torch.stack(return_result)
+            attention_result.append(attention_mask)
+        attention_result = torch.tensor(attention_result)
 
         result = {
-            "input_values": audios.to(torch.float32),
-            "audio_attention_mask": return_result.to(torch.float32),
+            "input_values": pad_result.to(torch.float32),
+            "audio_attention_mask": attention_result.to(torch.float32),
         }
 
         return result

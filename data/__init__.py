2353c5fb17096bb965261444546cc595292b942b
jp1924 <jp42maru@gmail.com>
2022-11-30 14:26:07 +0900

    Add: extractor

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from .collator import TorchCollator, TransducerCollator
-from .sampler import TorchSampler
+from .extractor import TransducerFeatureExtractor
 from .loader import get_concat_dataset
+from .sampler import TorchSampler

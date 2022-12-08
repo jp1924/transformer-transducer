7926eb41be80b65d129d89f40a1a4feb8a11dd4a
jp1924 <jp42maru@gmail.com>
2022-12-08 16:47:39 +0900

    Add: vocab None

--- model/config.py
+++ model/config.py
@@ -2,19 +2,19 @@ from transformers import PretrainedConfig
 
 
 class TransformerTransducerConfig(PretrainedConfig):
-    def __init__(self, vocab_size) -> None:
+    def __init__(self, vocab_size=None) -> None:
         self.label_layers = 2
         self.audio_layers = 18
 
-        self.hidden_size = 504
-        self.intermediate_size = 1024
+        self.hidden_size = 320
+        self.intermediate_size = 640
         self.activation_dropout = 0.03
         self.hidden_dropout = 0.03
         self.hidden_act = "gelu"
 
         self.attention_probs_dropout_prob = 0.03
         self.score_dropout = 0.07
-        self.num_attention_heads = 12
+        self.num_attention_heads = 10
 
         self.layer_norm_eps = 0.00001
         # self.attn_norm_eps = 0.00001
@@ -29,3 +29,6 @@ class TransformerTransducerConfig(PretrainedConfig):
         self.blank_id = 0
         self.is_audio = False
         self.mel_size = 80
+        self.model_test = True
+        self.pruned_heads = False
+        self.initializer_range = 0.02

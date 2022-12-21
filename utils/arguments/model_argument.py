502d16ee679634e9d5af9632e1c74bd6db2f3140
jp1924 <jp42maru@gmail.com>
2022-12-21 16:02:50 +0900

    Update: label_name > cache_dir

--- utils/arguments/model_argument.py
+++ utils/arguments/model_argument.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass, field
 
 @dataclass
 class ModelArguments:
-    num_labels: int = field(
-        default=1,
-        metadata={"help": "classification시 분류를 할 label의 개수를 설정합니다."},
+    cache_dir: str = field(
+        default="./cache",
+        metadata={"help": "모델 혹은 데이터를 저장할 캐쉬의 경로를 지정합니다."},
     )

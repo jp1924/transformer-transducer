2b50bc3e01a03c4c676716668551e028b03e8f7e
jp1924 <jp42maru@gmail.com>
2022-12-26 16:14:01 +0900

    Add: select number of process

--- utils/arguments/data_argument.py
+++ utils/arguments/data_argument.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass, field
 @dataclass
 class DataArguments:
     data_name: str = field(
-        default=None,
-        metadata={"help": "데이터으 허브 이름 혹은 데이터의 로컬 경로를 포함합니다."},
+        default="librispeech_asr",
+        metadata={"help": "데이터의 이름 혹은 데이터의 로컬 경로를 포함합니다."},
+    )
+    num_proc: int = field(
+        default=1,
+        metadata={"help": "전처리에서 사용할 CPU 프로세서의 개수를 설정합니다."},
     )

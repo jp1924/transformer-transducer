Fix: change direction of inequality sign

trainer.py
@@ -56,7 +56,7 @@ class TransducerTrainer(Seq2SeqTrainer):
 
         # [NOTE]: gaussian noise
         step = self.state.global_step
-        if step < 10000:
+        if step > 10000:
             # [NOTE]: copied from https://discuss.pytorch.org/t/is-there-any-way-to-add-noise-to-trained-weights/29829/2
             with torch.no_grad():
                 for param, name in zip(model.parameters(), model.named_parameters()):
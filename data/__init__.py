902a8b470145f2f29b0b681e28bc8f660f5b3c0a
jp1924 <jp42maru@gmail.com>
2022-12-30 22:48:34 +0900

    Remove: arrow loader

--- data/__init__.py
+++ data/__init__.py
@@ -2,4 +2,3 @@ from .collator import TransducerCollator
 from .processor import TransducerProcessor
 from .tokenizer import TransducerTokenizer
 from .extractor import TransducerFeatureExtractor
-from .loader import get_concat_dataset

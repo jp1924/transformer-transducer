bad764adfe8da7cc8289c466a7239be848ddba7b
jp1924 <jp42maru@gmail.com>
2023-01-07 00:20:29 +0900

    Add: fist predict

--- utils/arguments/train_argument.py
+++ utils/arguments/train_argument.py
@@ -12,3 +12,7 @@ class TransducerTrainArgument(Seq2SeqTrainingArguments):
         default=None,
         metadata={"help": "이건 나중에 삭제될 거임"},
     )
+    do_fist_predict: bool = field(
+        default=False,
+        metadata={"help": "만약 model을 checkpoint로 부터 불러오거나 재개 했을 때 모델의 성능을 측정하기 위해 사용하는 값"},
+    )

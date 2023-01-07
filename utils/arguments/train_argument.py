d67b1ef2d76eee012f0ceb5bc95a033a495e7438
jp1924 <jp42maru@gmail.com>
2023-01-08 01:57:27 +0900

    Remove: unusual argument

--- utils/arguments/train_argument.py
+++ utils/arguments/train_argument.py
@@ -4,10 +4,6 @@ from transformers import Seq2SeqTrainingArguments
 
 @dataclass
 class TransducerTrainArgument(Seq2SeqTrainingArguments):
-    mel_max_length: int = field(
-        default=400,
-        metadata={"help": "mel의 최대 길이르 지정합니다. 이 Transducer는 무조건 일정한 크기의 데이터만 들어가야 합니다."},
-    )
     vocab_path: str = field(
         default=None,
         metadata={"help": "이건 나중에 삭제될 거임"},

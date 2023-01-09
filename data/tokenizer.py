65866ec4022fe5bf5a387494df1930d1baa5cc5e
jp1924 <jp42maru@gmail.com>
2023-01-09 14:12:28 +0900

    Remove: delete unused outputs

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -18,41 +18,14 @@
 import json
 import os
 import sys
-from dataclasses import dataclass
-from itertools import groupby
 from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 from transformers.tokenization_utils import PreTrainedTokenizer, _insert_one_token_to_ordered_list
 from transformers.tokenization_utils_base import AddedToken
-from transformers.utils import ModelOutput, logging, to_py_obj
+from transformers.utils import logging, to_py_obj
 
 logger = logging.get_logger(__name__)
-
-ListOfDict = List[Dict[str, Union[int, str]]]
-
-
-@dataclass
-class TransformerTransducerTokenizerOutput(ModelOutput):
-    """
-    Output type of [` Wav2Vec2CTCTokenizer`], with transcription.
-
-    Args:
-        text (list of `str` or `str`):
-            Decoded logits in text from. Usually the speech transcription.
-        char_offsets (list of `List[Dict[str, Union[int, str]]]` or `List[Dict[str, Union[int, str]]]`):
-            Offsets of the decoded characters. In combination with sampling rate and model downsampling rate char
-            offsets can be used to compute time stamps for each charater. Total logit score of the beam associated with
-            produced text.
-        word_offsets (list of `List[Dict[str, Union[int, str]]]` or `List[Dict[str, Union[int, str]]]`):
-            Offsets of the decoded words. In combination with sampling rate and model downsampling rate word offsets
-            can be used to compute time stamps for each word.
-    """
-
-    text: Union[List[str], str]
-    char_offsets: Union[List[ListOfDict], ListOfDict] = None
-    word_offsets: Union[List[ListOfDict], ListOfDict] = None
-
 
 VOCAB_FILES_NAMES = {
     "vocab_file": "vocab.json",
@@ -73,63 +46,14 @@ PRETRAINED_VOCAB_FILES_MAP = {
 # Wav2Vec2 has no max input length
 PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {"jp42maru/transformer-transducer-960h": sys.maxsize}
 
-WAV2VEC2_KWARGS_DOCSTRING = r"""
-            padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `False`):
-                Activates and controls padding. Accepts the following values:
-                - `True` or `'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-                  sequence if provided).
-                - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
-                  acceptable input length for the model if that argument is not provided.
-                - `False` or `'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of different
-                  lengths).
-            max_length (`int`, *optional*):
-                Controls the maximum length to use by one of the truncation/padding parameters.
-                If left unset or set to `None`, this will use the predefined model maximum length if a maximum length
-                is required by one of the truncation/padding parameters. If the model has no specific maximum input
-                length (like XLNet) truncation/padding to a maximum length will be deactivated.
-            pad_to_multiple_of (`int`, *optional*):
-                If set will pad the sequence to a multiple of the provided value. This is especially useful to enable
-                the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta).
-            return_tensors (`str` or [`~utils.TensorType`], *optional*):
-                If set, will return tensors instead of list of python integers. Acceptable values are:
-                - `'tf'`: Return TensorFlow `tf.constant` objects.
-                - `'pt'`: Return PyTorch `torch.Tensor` objects.
-                - `'np'`: Return Numpy `np.ndarray` objects.
-            verbose (`bool`, *optional*, defaults to `True`):
-                Whether or not to print more information and warnings.
-"""
+TRANSFORMER_TRANSDUCER_KWARGS_DOCSTRING = r""""""
 
 
 # [NOTE]: Transformer-Transducer tokenizer is grpaheme tokenizer
 #         and it's must be
 # [NOTE]: copied from Wav2Vec2Tokenizer
 class TransformerTransducerTokenizer(PreTrainedTokenizer):
-    """
-    Constructs a Wav2Vec2CTC tokenizer.
-
-    This tokenizer inherits from [`PreTrainedTokenizer`] which contains some of the main methods. Users should refer to
-    the superclass for more information regarding such methods.
-
-    Args:
-        vocab_file (`str`):
-            File containing the vocabulary.
-        bos_token (`str`, *optional*, defaults to `"<s>"`):
-            The beginning of sentence token.
-        eos_token (`str`, *optional*, defaults to `"</s>"`):
-            The end of sentence token.
-        unk_token (`str`, *optional*, defaults to `"<unk>"`):
-            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
-            token instead.
-        pad_token (`str`, *optional*, defaults to `"<pad>"`):
-            The token used for padding, for example when batching sequences of different lengths.
-        word_delimiter_token (`str`, *optional*, defaults to `"|"`):
-            The token used for defining the end of a word.
-        do_lower_case (`bool`, *optional*, defaults to `False`):
-            Whether or not to accept lowercase input and lowercase the output when decoding.
-
-        **kwargs
-            Additional keyword arguments passed along to [`PreTrainedTokenizer`]
-    """
+    """"""
 
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
@@ -294,9 +218,7 @@ class TransformerTransducerTokenizer(PreTrainedTokenizer):
                 Will be passed to the underlying model specific decode method.
 
         Returns:
-            `List[str]` or [`~models.wav2vec2.tokenization_wav2vec2.TransformerTransducerTokenizerOutput`]: The list of decoded
-            sentences. Will be a [`~models.wav2vec2.tokenization_wav2vec2.TransformerTransducerTokenizerOutput`] when
-            `output_char_offsets == True` or `output_word_offsets == True`.
+            `List[str]`
         """
         batch_decoded = [
             self.decode(
@@ -315,92 +237,7 @@ class TransformerTransducerTokenizer(PreTrainedTokenizer):
         token_ids: Union[int, List[int], "np.ndarray", "torch.Tensor", "tf.Tensor"],
         skip_special_tokens: bool = False,
     ) -> str:
-        """
-        Converts a sequence of ids in a string, using the tokenizer and vocabulary with options to remove special
-        tokens and clean up tokenization spaces.
-
-        Similar to doing `self.convert_tokens_to_string(self.convert_ids_to_tokens(token_ids))`.
-
-        Args:
-            token_ids (`Union[int, List[int], np.ndarray, torch.Tensor, tf.Tensor]`):
-                List of tokenized input ids. Can be obtained using the `__call__` method.
-            skip_special_tokens (`bool`, *optional*, defaults to `False`):
-                Whether or not to remove special tokens in the decoding.
-            clean_up_tokenization_spaces (`bool`, *optional*, defaults to `True`):
-                Whether or not to clean up the tokenization spaces.
-            output_char_offsets (`bool`, *optional*, defaults to `False`):
-                Whether or not to output character offsets. Character offsets can be used in combination with the
-                sampling rate and model downsampling rate to compute the time-stamps of transcribed characters.
-
-                <Tip>
-
-                Please take a look at the example of [`~models.wav2vec2.tokenization_wav2vec2.decode`] to better
-                understand how to make use of `output_word_offsets`.
-
-                </Tip>
-
-            output_word_offsets (`bool`, *optional*, defaults to `False`):
-                Whether or not to output word offsets. Word offsets can be used in combination with the sampling rate
-                and model downsampling rate to compute the time-stamps of transcribed words.
-
-                <Tip>
-
-                Please take a look at the example of [`~models.wav2vec2.tokenization_wav2vec2.decode`] to better
-                understand how to make use of `output_word_offsets`.
-
-                </Tip>
-
-            kwargs (additional keyword arguments, *optional*):
-                Will be passed to the underlying model specific decode method.
-
-        Returns:
-            `str` or [`~models.wav2vec2.tokenization_wav2vec2.TransformerTransducerTokenizerOutput`]: The list of decoded
-            sentences. Will be a [`~models.wav2vec2.tokenization_wav2vec2.TransformerTransducerTokenizerOutput`] when
-            `output_char_offsets == True` or `output_word_offsets == True`.
-
-        Example:
-
-        ```python
-        >>> # Let's see how to retrieve time steps for a model
-        >>> from transformers import AutoTokenizer, AutoFeatureExtractor, AutoModelForCTC
-        >>> from datasets import load_dataset
-        >>> import datasets
-        >>> import torch
-
-        >>> # import model, feature extractor, tokenizer
-        >>> model = AutoModelForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-        >>> tokenizer = AutoTokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-        >>> feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-base-960h")
-
-        >>> # load first sample of English common_voice
-        >>> dataset = load_dataset("common_voice", "en", split="train", streaming=True)
-        >>> dataset = dataset.cast_column("audio", datasets.Audio(sampling_rate=16_000))
-        >>> dataset_iter = iter(dataset)
-        >>> sample = next(dataset_iter)
-
-        >>> # forward sample through model to get greedily predicted transcription ids
-        >>> input_values = feature_extractor(sample["audio"]["array"], return_tensors="pt").input_values
-        >>> logits = model(input_values).logits[0]
-        >>> pred_ids = torch.argmax(logits, axis=-1)
-
-        >>> # retrieve word stamps (analogous commands for `output_char_offsets`)
-        >>> outputs = tokenizer.decode(pred_ids, output_word_offsets=True)
-        >>> # compute `time_offset` in seconds as product of downsampling ratio and sampling_rate
-        >>> time_offset = model.config.inputs_to_logits_ratio / feature_extractor.sampling_rate
-
-        >>> word_offsets = [
-        ...     {
-        ...         "word": d["word"],
-        ...         "start_time": round(d["start_offset"] * time_offset, 2),
-        ...         "end_time": round(d["end_offset"] * time_offset, 2),
-        ...     }
-        ...     for d in outputs.word_offsets
-        ... ]
-        >>> # compare word offsets with audio `common_voice_en_100038.mp3` online on the dataset viewer:
-        >>> # https://huggingface.co/datasets/common_voice/viewer/en/train
-        >>> word_offsets[:3]
-        [{'word': 'WHY', 'start_time': 1.42, 'end_time': 1.54}, {'word': 'DOES', 'start_time': 1.64, 'end_time': 1.9}, {'word': 'MILISANDRA', 'start_time': 2.26, 'end_time': 2.9}]
-        ```"""
+        """"""
         # Convert inputs to python lists
         token_ids = to_py_obj(token_ids)
 

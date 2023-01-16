eb0f0be8bd9a4d609bb99e4e9c0d7bf3d147d097
jp1924 <jp42maru@gmail.com>
2023-01-17 08:42:46 +0900

    Add: init callback

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .arguments import DataArguments, TransducerTrainArgument, ModelArguments
 from .scheduler import get_tri_stage_scheduler_with_warmup
-from .noise import GaussianNoise
+from .noise import GaussianNoiseCallback

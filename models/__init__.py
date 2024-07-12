03f98618e618b675967431f691c9e58d63c2dde2
jp1924 <jp42maru@gmail.com>
2024-07-12 01:59:13 +0000

    Add: transformer adding

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,15 @@
+import importlib
+
 from .configuration_transformer_transducer import TransformerTransducerConfig, TransfoXLConfig
 from .feature_extraction_transformer_transducer import TransformerTransducerFeatureExtractor
 from .modeling_transformer_transducer import TransformerTransducerForRNNT, TransfoXLModel
 from .processing_transformer_transduceer import TransformerTransducerProcessor
+
+
+module = importlib.import_module("transformers")
+setattr(module, "TransformerTransducerConfig", TransformerTransducerConfig)
+setattr(module, "TransformerTransducerFeatureExtractor", TransformerTransducerFeatureExtractor)
+setattr(module, "TransformerTransducerForRNNT", TransformerTransducerForRNNT)
+setattr(module, "TransformerTransducerProcessor", TransformerTransducerProcessor)
+setattr(module, "TransfoXLConfig", TransfoXLConfig)
+setattr(module, "TransfoXLModel", TransfoXLModel)

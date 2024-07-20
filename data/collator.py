c9c4c86d4862e6caab133aece622301c37563b31
jp1924 <jp42maru@gmail.com>
2024-07-20 19:20:23 +0000

    Add: spec_aug

--- data/collator.py
+++ data/collator.py
@@ -1,6 +1,5 @@
-import math
 from dataclasses import dataclass
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -8,71 +7,197 @@ import torch
 from transformers.data.data_collator import DataCollatorMixin
 
 
-# from ..models import TransformerTransducerProcessor
+def _compute_mask_indices(
+    shape: Tuple[int, int],
+    mask_prob: float,
+    mask_length: int,
+    attention_mask: Optional[torch.LongTensor] = None,
+    min_masks: int = 0,
+) -> np.ndarray:
+    """
+    Computes random mask spans for a given shape. Used to implement [SpecAugment: A Simple Data Augmentation Method for
+    ASR](https://arxiv.org/abs/1904.08779). Note that this method is not optimized to run on TPU and should be run on
+    CPU as part of the preprocessing during training.
+
+    Args:
+        shape: The shape for which to compute masks. This should be of a tuple of size 2 where
+               the first element is the batch size and the second element is the length of the axis to span.
+        mask_prob:  The percentage of the whole axis (between 0 and 1) which will be masked. The number of
+                    independently generated mask spans of length `mask_length` is computed by
+                    `mask_prob*shape[1]/mask_length`. Note that due to overlaps, `mask_prob` is an upper bound and the
+                    actual percentage will be smaller.
+        mask_length: size of the mask
+        min_masks: minimum number of masked spans
+        attention_mask: A (right-padded) attention mask which independently shortens the feature axis of
+                        each batch dimension.
+    """
+    batch_size, sequence_length = shape
+
+    if mask_length < 1:
+        raise ValueError("`mask_length` has to be bigger than 0.")
+
+    if mask_length > sequence_length:
+        raise ValueError(
+            f"`mask_length` has to be smaller than `sequence_length`, but got `mask_length`: {mask_length}"
+            f" and `sequence_length`: {sequence_length}`"
+        )
+
+    # epsilon is used for probabilistic rounding
+    epsilon = np.random.rand(1).item()
+
+    def compute_num_masked_span(input_length):
+        """Given input length, compute how many spans should be masked"""
+        num_masked_span = int(mask_prob * input_length / mask_length + epsilon)
+        num_masked_span = max(num_masked_span, min_masks)
+
+        # make sure num masked span <= sequence_length
+        if num_masked_span * mask_length > sequence_length:
+            num_masked_span = sequence_length // mask_length
+
+        # make sure num_masked span is also <= input_length - (mask_length - 1)
+        if input_length - (mask_length - 1) < num_masked_span:
+            num_masked_span = max(input_length - (mask_length - 1), 0)
+
+        return num_masked_span
+
+    # compute number of masked spans in batch
+    input_lengths = (
+        attention_mask.sum(-1).detach().tolist()
+        if attention_mask is not None
+        else [sequence_length for _ in range(batch_size)]
+    )
+
+    # SpecAugment mask to fill
+    spec_aug_mask = np.zeros((batch_size, sequence_length), dtype=bool)
+    spec_aug_mask_idxs = []
+
+    max_num_masked_span = compute_num_masked_span(sequence_length)
+
+    if max_num_masked_span == 0:
+        return spec_aug_mask
+
+    for input_length in input_lengths:
+        # compute num of masked spans for this input
+        num_masked_span = compute_num_masked_span(input_length)
+
+        # get random indices to mask
+        spec_aug_mask_idx = np.random.choice(
+            np.arange(input_length - (mask_length - 1)), num_masked_span, replace=False
+        )
+
+        # pick first sampled index that will serve as a dummy index to pad vector
+        # to ensure same dimension for all batches due to probabilistic rounding
+        # Picking first sample just pads those vectors twice.
+        if len(spec_aug_mask_idx) == 0:
+            # this case can only happen if `input_length` is strictly smaller then
+            # `sequence_length` in which case the last token has to be a padding
+            # token which we can use as a dummy mask id
+            dummy_mask_idx = sequence_length - 1
+        else:
+            dummy_mask_idx = spec_aug_mask_idx[0]
+
+        spec_aug_mask_idx = np.concatenate(
+            [spec_aug_mask_idx, np.ones(max_num_masked_span - num_masked_span, dtype=np.int32) * dummy_mask_idx]
+        )
+        spec_aug_mask_idxs.append(spec_aug_mask_idx)
+
+    spec_aug_mask_idxs = np.array(spec_aug_mask_idxs)
+
+    # expand masked indices to masked spans
+    spec_aug_mask_idxs = np.broadcast_to(
+        spec_aug_mask_idxs[:, :, None], (batch_size, max_num_masked_span, mask_length)
+    )
+    spec_aug_mask_idxs = spec_aug_mask_idxs.reshape(batch_size, max_num_masked_span * mask_length)
+
+    # add offset to the starting indexes so that indexes now create a span
+    offsets = np.arange(mask_length)[None, None, :]
+    offsets = np.broadcast_to(offsets, (batch_size, max_num_masked_span, mask_length)).reshape(
+        batch_size, max_num_masked_span * mask_length
+    )
+    spec_aug_mask_idxs = spec_aug_mask_idxs + offsets
+
+    # ensure that we cannot have indices larger than sequence_length
+    if spec_aug_mask_idxs.max() > sequence_length - 1:
+        spec_aug_mask_idxs[spec_aug_mask_idxs > sequence_length - 1] = sequence_length - 1
+
+    # scatter indices to mask
+    np.put_along_axis(spec_aug_mask, spec_aug_mask_idxs, 1, -1)
+
+    return spec_aug_mask
 
 
 @dataclass
 class DataCollatorRNNTWithPadding(DataCollatorMixin):
+    model: "TransformerTransducerForRNNT"
     processor: "TransformerTransducerProcessor"
     sampling_rate: int = 16000
     padding: Union[bool, str] = "longest"
     return_tensors: str = "pt"
 
-    def get_audio_chunk_ls(self, input_features):
-        chunk_num = math.ceil(len(input_features) / self.sampling_rate) * self.sampling_rate
-
-        chunk_idxer = range(0, chunk_num, self.sampling_rate)
-        chunk_audio_ls = list()
-        for i in chunk_idxer:
-            chunk_audio = input_features[i : i + self.sampling_rate]
-
-            # mel로 변환할 때 음성의 길이가 너무 짧으면 processor에서 error가 발생 함.
-            if chunk_audio.shape[0] < self.processor.feature_extractor.n_fft:
-                padded_array = np.zeros(self.processor.feature_extractor.n_fft)
-                padded_array[: chunk_audio.shape[0]] = chunk_audio
-                chunk_audio = padded_array
-
-            chunk_audio_ls.append(chunk_audio)
-        # chunk_audio_ls = [input_features[i : i + self.sampling_rate] for i in chunk_idxer]
-        return chunk_audio_ls
-
     def torch_call(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         labels = [{"input_ids": feature["labels"]} for feature in features]
+        input_features = [
+            {"input_features": self._mask_hidden_states(feature["input_features"])[0]}
+            for feature in features
+        ]
 
-        feature_ls = list()
-        mask_ls = list()
-        check_ls = list()
-        for feature in features:
-            input_features = feature["input_features"].numpy()
-            chunk_audio_ls = self.get_audio_chunk_ls(input_features)
-
-            # torch input 입력하는 경우 error가 발생 함.
-            pro_outputs = self.processor(
-                audio=chunk_audio_ls,
-                sampling_rate=self.sampling_rate,
-                return_tensors=self.return_tensors,
-            )
-            check_ls.append(pro_outputs["attention_mask"].sum(-1))
-
-            flatten_input_features = np.vstack(pro_outputs["input_features"])
-            flatten_attention_mask = np.hstack(pro_outputs["attention_mask"])
-
-            feature_ls.append({"input_features": flatten_input_features})
-            mask_ls.append({"input_ids": flatten_attention_mask})
-
-        batch = self.processor.pad(input_features=feature_ls, return_tensors=self.return_tensors)
-        batch["attention_mask"] = self.processor.pad(labels=mask_ls, return_tensors=self.return_tensors)["input_ids"]
-
-        max_shape = batch["attention_mask"].sum(-1).max()
-        batch["attention_mask"] = batch["attention_mask"][:, :max_shape]
-        batch["input_features"] = batch["input_features"][:, :max_shape]
-
-        _labels = self.processor.pad(labels=labels, return_tensors=self.return_tensors)
-
-        batch["labels"] = _labels["input_ids"]
-        batch["decoder_attention_mask"] = _labels["attention_mask"]
-
+        batch = self.processor.pad(
+            labels=labels,
+            input_features=input_features,
+            padding=self.padding,
+            return_tensors=self.return_tensors,
+        )
         batch = {k: v.to(torch.float32) for k, v in batch.items()}
         batch["labels"] = batch["labels"].to(torch.long)
 
         return batch
+
+    def _mask_hidden_states(
+        self,
+        hidden_states: torch.FloatTensor,
+        mask_time_indices: Optional[torch.FloatTensor] = None,
+        attention_mask: Optional[torch.LongTensor] = None,
+    ):
+        """
+        Masks extracted features along time axis and/or along feature axis according to
+        [SpecAugment](https://arxiv.org/abs/1904.08779).
+        """
+        config = self.model.config.audio_config
+
+        # `config.apply_spec_augment` can set masking to False
+        if not getattr(config, "apply_spec_augment", True):
+            return hidden_states
+
+        if len(hidden_states.shape) == 2:
+            hidden_states = hidden_states.unsqueeze(0)
+
+        # generate indices & apply SpecAugment along time axis
+        batch_size, sequence_length, hidden_size = hidden_states.size()
+
+        if mask_time_indices is not None:
+            # apply SpecAugment along time axis with given mask_time_indices
+            hidden_states[mask_time_indices] = 0
+        elif config.mask_time_prob > 0 and self.model.training:
+            mask_time_indices = _compute_mask_indices(
+                (batch_size, sequence_length),
+                mask_prob=config.mask_time_prob,
+                mask_length=config.mask_time_length,
+                attention_mask=attention_mask,
+                min_masks=config.mask_time_min_masks,
+            )
+            mask_time_indices = torch.tensor(mask_time_indices, device=hidden_states.device, dtype=torch.bool)
+            hidden_states[mask_time_indices] = 0
+
+        if config.mask_feature_prob > 0 and self.model.training:
+            # generate indices & apply SpecAugment along feature axis
+            mask_feature_indices = _compute_mask_indices(
+                (batch_size, hidden_size),
+                mask_prob=config.mask_feature_prob,
+                mask_length=config.mask_feature_length,
+                min_masks=config.mask_feature_min_masks,
+            )
+            mask_feature_indices = torch.tensor(mask_feature_indices, device=hidden_states.device, dtype=torch.bool)
+            mask_feature_indices = mask_feature_indices[:, None].expand(-1, sequence_length, -1)
+            hidden_states[mask_feature_indices] = 0
+
+        return hidden_states
